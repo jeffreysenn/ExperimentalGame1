c34e8c6a6c3058fec5a83899e7c67631a4916587
Antonio <antonioh.ackalin@hotmail.com>
2019-01-23 13:46:29 +0100

    HENLO FRENDS

---
+++ Assets/Pillar.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PillarStates
+{
+    Intact,
+    Cracked,
+    Fractured,
+    Wrecked,
+    Destroyed,
+    Count
+}
+
+public class Pillar : MonoBehaviour
+{
+    public PillarStates m_state;
+
+    public bool IsDestructing
+    {
+        get { return (m_state > 0); }
+    }
+
+    private float m_destructDuration = 2f;
+    private float m_destructTime;
+
+    void Start()
+    {
+        ResetDestructTime();
+    }
+
+    void Update()
+    {
+        if (IsDestructing && m_state != PillarStates.Destroyed)
+        {
+            m_destructTime -= Time.deltaTime;
+            if (m_destructTime <= 0)
+            {
+                m_state = m_state + 1;
+                Debug.Log(m_state);
+                m_destructTime = m_destructDuration;
+            }
+        }
+
+        if (GetComponent<SpriteRenderer>() != null)
+            GetComponent<SpriteRenderer>().color = new Color(
+                GetComponent<SpriteRenderer>().color.r,
+                GetComponent<SpriteRenderer>().color.g,
+                GetComponent<SpriteRenderer>().color.b,
+                1 - ((float)m_state/((float)PillarStates.Count-1)));
+        //Debug.Log(((1/5)));
+    }
+
+    public void TriggerDestruction()
+    {
+        m_state = PillarStates.Cracked;
+    }
+
+    public void Repair()
+    {
+        m_state = PillarStates.Intact;
+    }
+
+    public void ResetDestructTime()
+    {
+        m_destructTime = m_destructDuration;
+    }
+}
---
+++ Assets/PillarManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PillarManager : MonoBehaviour
+{
+    public Pillar[] m_pillars;
+
+    public float m_activationFrequency;
+    private float m_activationTime;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        m_activationTime += Time.deltaTime;
+        if (m_activationTime >= m_activationFrequency)
+        {
+            m_activationTime = 0;
+            CrackRandomPillar();
+        }
+    }
+
+    void CrackRandomPillar()
+    {
+        if (m_pillars.All(m_pillar => m_pillar.IsDestructing))
+            return;
+
+
+        List<Pillar> m_intactPillars = new List<Pillar>();
+
+        foreach (Pillar m_pillar in m_pillars)
+        {
+            if (!m_pillar.IsDestructing)
+            {
+                m_intactPillars.Add(m_pillar);
+            }
+        }
+        int m_random = Random.Range(0, m_intactPillars.Count);
+
+        m_intactPillars[m_random].TriggerDestruction();
+    }
+}
